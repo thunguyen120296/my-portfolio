858329eda0e354dfb3376f4a330eca89598710f4
Thu Nguyen <thunguyen@gmail.com>
2022-05-03 20:47:05 +0700

    my-portfolio-website

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import About from "./components/about/About";
+function App(){
+    return (
+        <div>
+            <About />
+        </div>
+    )
+}
+export default App;
---
+++ src/components/about/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './about.css';
+function About(){
+    return(
+        <div className='about'>
+            <h1>About</h1>
+        </div>
+    )
+}
+export default About;
