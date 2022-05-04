f7081fe37a2f6932f02c1e35f9d95a6797496ec7
Nguyen Le Anh Thu <thu.nla@movi.vn>
2022-05-04 17:53:22 +0700

    update link navigation

--- src/components/partials/navigation/Navigation.jsx
+++ src/components/partials/navigation/Navigation.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import './navigation.css';
+import About from '../../about/About';
 import {BiHome, BiUserCircle, BiBookHeart, BiGridAlt, BiMessageRoundedDots} from 'react-icons/bi';
 function Navigation(){
     return (
         <ul className='navigation'>
+            <li><Link to='/'>123</Link></li>
+            <li><Link to='/about'>123</Link></li>
             <li><a href='/' className='item__link'><BiHome /></a></li>
             <li><a href="/about" className='item__link'><BiUserCircle /></a></li>
             <li><a href="/experience" className='item__link'><BiBookHeart /></a></li>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/partials/navigation/Navigation';
+import About from './components/about/About';
 import App from './App';
 import './index.css';
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <Navigation />
+            <Routes>
+                <Route path='/' />
+            </Routes>
+        </Router>
+    </React.StrictMode>, 
+    document.getElementById('root'));
